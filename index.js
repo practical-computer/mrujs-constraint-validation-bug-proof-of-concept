7f03da82e7e3b707df751fd4e95aa6fc026aa5fa
Thomas Cannon <tcannon00@gmail.com>
2026-01-28 07:26:27 -0500

    Example of how `re-enable` on `invalid` causes unexpected unlocks

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ function formSubmit(event) {
   console.debug(["🥰", event])
 }
 
+function invalidHandler(event) {
+  console.debug(["⚠️", event])
+}
+
+function toggleLocks(event) {
+  document.querySelectorAll(`.button-to-lock`).forEach(x => x.toggleAttribute(`disabled`))
+}
+
 document.querySelectorAll(`button,input[type=submit]`).forEach(x => x.addEventListener(`click`, clickHandler))
 document.querySelectorAll(`form`).forEach(x => x.addEventListener(`submit`, formSubmit))
+document.querySelectorAll(`form`).forEach(x => x.addEventListener(`invalid`, invalidHandler))
+
+document.getElementById(`toggle-lock`).addEventListener(`click`, toggleLocks)
